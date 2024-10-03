8bbf26ebe31b5e7ba553dc6e2dd9967e4933cdde
RezaPratama <63398405+HillarioRZA@users.noreply.github.com>
2024-10-04 01:33:45 +0700

    Delete snapshare directory

--- snapshare/includes/footer.php
+++
@@ -1,8 +0,0 @@
-
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
-</body>
-</html>
